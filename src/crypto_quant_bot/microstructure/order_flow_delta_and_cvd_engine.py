bd2f45f27d9d7db7bb2f7a36cc55a98e497e6183
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-17 23:56:59 +0200

    refactor(lot45): keep policy parameters while hardening artifacts

--- src/crypto_quant_bot/microstructure/order_flow_delta_and_cvd_engine.py
+++ src/crypto_quant_bot/microstructure/order_flow_delta_and_cvd_engine.py
@@ -1,15 +1,9 @@
 from __future__ import annotations
 
-from dataclasses import dataclass
 from typing import Any
 
 from . import _order_flow_delta_and_cvd_engine_impl as _impl
 from .order_flow_delta_and_cvd_engine_models import _internal_checksum_construction
-from .order_flow_delta_and_cvd_engine_policy import (
-    MAX_INPUT_AGE_US,
-    MAX_UNKNOWN_VOLUME_RATIO,
-)
-from .order_flow_delta_and_cvd_engine_validation import require
 
 CONFIG_PATH = _impl.CONFIG_PATH
 STATE_PATH = _impl.STATE_PATH
@@ -27,27 +21,11 @@ ZERO_SHA256 = _impl.ZERO_SHA256
 CALCULATION_DECIMAL_ROUNDING = _impl.CALCULATION_DECIMAL_ROUNDING
 CODE_BOUND_PATHS = _impl.CODE_BOUND_PATHS
 LOT45_REASON_CODES = _impl.LOT45_REASON_CODES
+OrderFlowPolicy = _impl.OrderFlowPolicy
 canonical_checksum = _impl.canonical_checksum
 file_checksum = _impl.file_checksum
 load_json_object = _impl.load_json_object
 atomic_write_json = _impl.atomic_write_json
-
-
-@dataclass(frozen=True, slots=True)
-class OrderFlowPolicy(_impl.OrderFlowPolicy):
-    def __post_init__(self) -> None:
-        super().__post_init__()
-        require(
-            self.max_input_age_us == MAX_INPUT_AGE_US,
-            "Lot45 max input age changed",
-        )
-        require(
-            self.max_unknown_volume_ratio == MAX_UNKNOWN_VOLUME_RATIO,
-            "Lot45 max unknown-volume ratio changed",
-        )
-
-
-setattr(_impl, "OrderFlowPolicy", OrderFlowPolicy)
 
 
 def build_order_flow(classified_trades: Any, policy: OrderFlowPolicy) -> Any:
