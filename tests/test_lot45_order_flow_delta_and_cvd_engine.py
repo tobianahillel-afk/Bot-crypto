88c190e734e83d9a51c41ef2c27f8c930ef8f20b
tobianahillel-afk <269188945+tobianahillel-afk@users.noreply.github.com>
2026-08-17 11:22:28 +0200

    test(lot45): bind reference build to hardened code tree

--- tests/test_lot45_order_flow_delta_and_cvd_engine.py
+++ tests/test_lot45_order_flow_delta_and_cvd_engine.py
@@ -32,7 +32,7 @@ from crypto_quant_bot.microstructure.trades_and_aggressor_classification_schema_
 ROOT = Path(__file__).resolve().parents[1]
 ZERO_SHA256 = "0" * 64
 QUOTE_SHA256 = "1" * 64
-REFERENCE_CODE_TREE_SHA = "cf45e466373459ebcb5530a1116d954e33aff6d3"
+REFERENCE_CODE_TREE_SHA = "953bf016f4483f5082786febc70c0efd6b9c6328"
 
 
 def _policy(*, unknown_ratio: str = "1") -> OrderFlowPolicy:
